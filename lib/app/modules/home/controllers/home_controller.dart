ca1c8f8ba1461f6318135a2cd4041f2fec610fb5
Waleed Makki <72141353+WelliMakki@users.noreply.github.com>
2023-08-27 13:55:11 +0300

    Update home_controller.dart

--- lib/app/modules/home/controllers/home_controller.dart
+++ lib/app/modules/home/controllers/home_controller.dart
@@ -5,30 +5,28 @@ import 'package:themer/app/manager/controllers/manager_controller.dart';
 class HomeController extends GetxController {
   RxBool isDark = false.obs;
 
-  //Instance of themeMode
+  
   ThemeMode themeMode = ThemeMode.system;
 
-  //Change Theme to Light
+  
   changeThemeToLight() {
-    //Set [themeMode] to light
+  
     themeMode = ThemeMode.light;
 
-    //trigger theme mode to Theme Manager so it can be applied
     Get.find<ManagerController>().changeTheme(themeMode);
 
-    //set isDark variable to false
     isDark(false);
   }
 
-  //Change Theme to Dark
+
   changeThemeToDark() {
-    //Set [themeMode] to dark
+
     themeMode = ThemeMode.dark;
 
-    //trigger theme mode to Theme Manager so it can be applied
+
     Get.find<ManagerController>().changeTheme(themeMode);
 
-    //set isDark variable to true
+
     isDark(true);
   }
 }
